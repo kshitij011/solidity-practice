7242a9d0ecdd09ce15d185cffcf59b31865e0e6d
Kshitij Bhoite <kshitijbhoite011@gmail.com>
2024-06-13 22:41:54 +0530

    enhancing data-types in llibrary

--- Advanced/Library.sol
+++ Advanced/Library.sol
@@ -14,3 +14,27 @@ contract Test {
         return Math.max(x, y);
     }
 }
+
+library ArrayLib {
+    function find(uint[] storage arr, uint x) public view returns (uint) {
+        for (uint i = 0; i <= arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+
+        revert("not found");
+    }
+}
+
+contract TestArray {
+    // it specifies that for all arrays of data-type uint[], attach all of the functionalities defined inside library.
+    using ArrayLib for uint[];
+    uint[] public arr = [1, 2, 3];
+
+    function testFind() external view returns (uint i) {
+        // return ArrayLib.find(arr, 2);
+        //alternative syntax:
+        return arr.find(2);
+    }
+}
